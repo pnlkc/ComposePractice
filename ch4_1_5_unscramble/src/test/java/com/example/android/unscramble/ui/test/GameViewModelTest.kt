3813e72d1fe7a793073a6a3b36ba30cf280c4cda
Jichan Choi <pnlkc@naver.com>
2023-02-24 17:03:08 +0900

    230224

---
+++ ch4_1_5_unscramble/src/test/java/com/example/android/unscramble/ui/test/GameViewModelTest.kt
@@ -0,0 +1,95 @@
+package com.example.android.unscramble.ui.test
+
+import com.example.android.unscramble.data.MAX_NO_OF_WORDS
+import com.example.android.unscramble.data.SCORE_INCREASE
+import com.example.android.unscramble.data.getUnscrambledWord
+import com.example.android.unscramble.ui.GameViewModel
+import junit.framework.TestCase.*
+import org.junit.Assert.assertNotEquals
+import org.junit.Test
+
+// 테스트 메소드는 개별적으로 실행되어 각 테스트마다 테스트 클래스의 새로운 인스턴스를 생성
+class GameViewModelTest {
+    companion object {
+        private const val SCORE_AFTER_FIRST_CORRECT_ANSWER = SCORE_INCREASE
+    }
+
+    private val viewModel = GameViewModel()
+
+    // 성공 경로
+    // 테스트 함수 이름 규칙 thingUnderTest_TriggerOfTest_ResultOfTest
+    @Test
+    fun gameViewModel_CorrectWordGuessed_ScoreUpdatedAndErrorFlagUnset() {
+        var currentGameUiState = viewModel.uiState.value
+        val correctPlayerWord = getUnscrambledWord(currentGameUiState.currentScrambledWord)
+        viewModel.updateUserGuess(correctPlayerWord)
+        viewModel.checkUserGuess()
+
+        currentGameUiState = viewModel.uiState.value
+        assertFalse(currentGameUiState.isGuessedWordWrong)
+        assertEquals(SCORE_AFTER_FIRST_CORRECT_ANSWER, currentGameUiState.score)
+    }
+
+    // 오류 경로
+    @Test
+    fun gameViewModel_IncorrectGuess_ErrorFlagSet() {
+        val incorrectPlayerWord = "and"
+        viewModel.updateUserGuess(incorrectPlayerWord)
+        viewModel.checkUserGuess()
+        val currentGameUiState = viewModel.uiState.value
+        assertTrue(currentGameUiState.isGuessedWordWrong)
+        assertEquals(0, currentGameUiState.score)
+    }
+
+    // 경계 사례1 (ui의 초기 상태 테스트)
+    @Test
+    fun gameViewModel_Initialization_FirstWordLoaded() {
+        val gameUiState = viewModel.uiState.value
+        val unScrambledWord = getUnscrambledWord(gameUiState.currentScrambledWord)
+
+        assertNotEquals(unScrambledWord, gameUiState.currentScrambledWord)
+        assertTrue(gameUiState.currentWordCount == 1)
+        assertTrue(gameUiState.score == 0)
+        assertFalse(gameUiState.isGuessedWordWrong)
+        assertFalse(gameUiState.isGameOver)
+    }
+
+    // 경계 사례2 (게임이 끝난 후 ui 상태 테스트)
+    @Test
+    fun gameViewModel_AllWordGuessed_UiStateUpdatedCorrectly() {
+        var expectedScore = 0
+        var currentGameUiState = viewModel.uiState.value
+        var correctPlayerWord = getUnscrambledWord(currentGameUiState.currentScrambledWord)
+        repeat(MAX_NO_OF_WORDS) {
+            expectedScore += SCORE_INCREASE
+            viewModel.updateUserGuess(correctPlayerWord)
+            viewModel.checkUserGuess()
+
+            currentGameUiState = viewModel.uiState.value
+            correctPlayerWord = getUnscrambledWord(currentGameUiState.currentScrambledWord)
+
+            assertEquals(expectedScore, currentGameUiState.score)
+        }
+
+        assertEquals(MAX_NO_OF_WORDS + 1, currentGameUiState.currentWordCount)
+        assertTrue(currentGameUiState.isGameOver)
+    }
+
+    // 테스크 코드 적용 범위 개선용 코드
+    @Test
+    fun gameViewModel_WordSkipped_ScoreUnchangedAndWordCountIncreased() {
+        var currentGameUiState = viewModel.uiState.value
+        val correctPlayerWord = getUnscrambledWord(currentGameUiState.currentScrambledWord)
+        viewModel.updateUserGuess(correctPlayerWord)
+        viewModel.checkUserGuess()
+
+        currentGameUiState = viewModel.uiState.value
+        val lastWordCount = currentGameUiState.currentWordCount
+        viewModel.skipWord()
+        currentGameUiState = viewModel.uiState.value
+        // Assert that score remains unchanged after word is skipped.
+        assertEquals(SCORE_AFTER_FIRST_CORRECT_ANSWER, currentGameUiState.score)
+        // Assert that word count is increased by 1 after word is skipped.
+        assertEquals(lastWordCount + 1, currentGameUiState.currentWordCount)
+    }
+}
