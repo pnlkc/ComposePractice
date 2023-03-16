242e99a8a9a5a793e5fc04d4a9618d958f4b7856
Jichan Choi <pnlkc@naver.com>
2023-03-16 16:31:50 +0900

    230316

--- ch6_2_4_compose_training_inventory/src/main/java/com/example/inventory/data/Item.kt
+++ ch6_2_4_compose_training_inventory/src/main/java/com/example/inventory/data/Item.kt
@@ -20,16 +20,17 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 /**
- * Entitiy (개체, 독립체)
+ * Entity (개체, 독립체)
  * 데이터베이스에 저장될 데이터의 형태를 가지는 개체 (Data class 형태)
  *
  * @Entity 어노테이션을 통해 Item 데이터 클래스가 room의 Entitiy임을 표시
- * tableName 값으로 테이블의 이름을 설정 (기본값은 data class의 이름 소문자화, Item -> item)
+ * tableName 값으로 테이블의 이름을 설정 (기본값은 data class의 이름)
  *
  * @PrimaryKey는 테이블의 모든 항목을 식별할 수 있는 고유키로 반드시 필요
  * autoGenerate = true로 설정하면 각 항목에 자동으로 고유키가 부여됨
  */
-@Entity(tableName = "items")
+//@Entity(tableName = "items")
+@Entity
 data class Item(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name: String,
